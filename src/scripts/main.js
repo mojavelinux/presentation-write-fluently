9880a6a16025d1cb7a3ea21f5868e54ba32007f6
Dan Allen <dan.j.allen@gmail.com>
2015-11-09 23:54:06 -0700

    make it so

---
+++ src/scripts/main.js
@@ -0,0 +1,26 @@
+var bespoke = require('bespoke'),
+  //bullets = require('bespoke-bullets'),
+  classes = require('bespoke-classes'),
+  cursor = require('bespoke-cursor'),
+  forms = require('bespoke-forms'),
+  fullscreen = require('bespoke-fullscreen'),
+  hash = require('bespoke-hash'),
+  nav = require('bespoke-nav'),
+  overview = require('bespoke-overview'),
+  scale = require('bespoke-scale'),
+  isWebKit = 'webkitAppearance' in document.documentElement.style;
+
+var deck = bespoke.from('.deck', [
+  classes(), // classes() must be first as it provides critical foundation for other plugins
+  scale(isWebKit ? 'zoom' : 'transform'),
+  nav(),
+  fullscreen(),
+  overview(),
+  //bullets('li, .bullet'),
+  hash(),
+  //cursor(),
+  forms()
+]);
+
+// expose deck API to applications
+window.deck = deck;
